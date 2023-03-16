a84436cd831e2ac46d5f384caea1c36cb3f014fd
Shahriyar Ramezani <shahriyarramezani@ShahriyarsMBP2.lan>
2023-03-16 05:30:01 +0000

    worked on a few more areas to further build the chatapplicaton, but somehow got stuck with a lot of errors

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-const Message = () => {
+
+const Message = ({ message }) => {
+  const [user] = useAuthState(auth);
+
   return (
-    <div className={`chat-bubble`}>
-      <img className="chat-bubble__left" src="" alt="user avatar" />
+    <div className={`chat-bubble ${message.uid === user.uid ? "right" : ""}`}>
+      <img className="chat-bubble__left" src={message.avatar} alt="user avatar" />
       <div className="chat-bubble__right">
-        <p className="user-name">Timonwa Akintokun</p>
-        <p className="user-message">
-          We are building a real time chat app with React and Firebase.
+
+      {/* Add properties at some point, username and message */}
+        <p className="user-name">{message.name}</p>
+        <p className="user-message">{message.text}
         </p>
       </div>
     </div>
   );
 };
+
+
+
 
 export default Message;
